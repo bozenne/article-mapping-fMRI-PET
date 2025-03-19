33cd37022cca4e388b51c374484cbd27fc60edf4
fishpm <patrick.fisher@gmail.com>
2025-03-19 15:23:57 +0100

    Update figure1.R
    updated plot features for figure1

--- figure1.R
+++ figure1.R
@@ -25,24 +25,24 @@ region.name <- readRDS("results/regionName.rds")
 
 ## * prepare figure
 nsig <- 2
-sub.text.corr <- paste0("Pearson's rho: ", signif(strategy1.cor$estimate,nsig),
-                        ', 95% CI: ', paste(signif(strategy1.cor$conf.int,nsig), collapse = '; '),
-                        ', p = ', signif(strategy1.cor$p.value,nsig))
+sub.text.corr <- paste0('\u03c1 = ', signif(l_corr$estimate,nsig),
+                   ' [', paste(str_pad(signif(l_corr$conf.int,2), width = 4, pad = '0', side = 'right'), collapse = '; '),
+                   ']\np = ', signif(l_corr$p.value,nsig))
 
 figure1 <- ggplot(cbind(df.2cohorts, region.full = region.name[df.2cohorts$region]), aes(x = pet, y = asl)) 
 figure1 <- figure1 + geom_smooth(method = 'lm', alpha = .8, color = 'black') 
-figure1 <- figure1 + geom_point(aes(color = region)) 
+figure1 <- figure1 + geom_point(aes(color = region, size = 5)) 
 figure1 <- figure1 + theme_classic() 
 figure1 <- figure1 + theme(axis.text = element_text(size=20),
                            axis.title = element_text(size=24,face="bold"), 
                            legend.position = 'none',
-                           plot.title = element_text(hjust = 0.5, face = "bold", size = 24),
                            plot.subtitle = element_text(hjust = 0.5, size = 20))
-figure1 <- figure1 + labs(x = '[11C]Cimbi-36 BPND', y = 'Baseline CBF (ml/100g/min)', title = '5-HT2AR vs. CBF', subtitle = sub.text.corr)
+figure1 <- figure1 + labs(x = '[11C]Cimbi-36 BPND', y = 'Baseline CBF (ml/100g/min)')
 figure1 <- figure1 + scale_x_continuous(breaks = seq(0.2,1.6,0.2))
 figure1 <- figure1 + scale_y_continuous(limits = c(35,70), breaks = seq(35,70,5))
+figure1 <- figure1 + annotate("label", x = 0.3, y = 65, label = sub.text.corr, hjust = 0, vjust = 0, size = 7)
 figure1 <- figure1 + geom_label_repel(aes(label = region.full, 
-                                          fill = region), size = 5)
+                                          fill = region), size = 4)
 figure1
 
 ## * export
